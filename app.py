1e1c8c8e49440499e0eda3310f5c0a22c061553e
mohitkhyalia <70964926+mohitkhyalia@users.noreply.github.com>
2021-04-29 12:00:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def film():
 
 @app.route('/music')
 def musicpg():
-    return(render_template("ok.html"))
+    return(render_template("Ok.html"))
 
 @app.route('/out', methods=['GET','POST'])
 def music():
